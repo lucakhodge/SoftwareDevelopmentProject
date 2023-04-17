2da32d6775db6ce1016d94cbdebe767b2ad1a51a
Aidan Reese <aire2100@colorado.edu>
2023-04-17 12:49:48 -0600

    Adding initial table schema for Users, StudyGuides and Reviews

--- ProjectCodeComponents/init_data/info.sql
+++ ProjectCodeComponents/init_data/info.sql
@@ -1,4 +1,27 @@
-CREATE TABLE users(
- username VARCHAR(50) PRIMARY KEY,
- password CHAR(60) NOT NULL
+CREATE TABLE Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,  /* Right now ID auto increments, could lead to secuirity risks */
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE StudyGuides (
+    SG_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    user_id INT,
+    rating INT, /* This is up for change,  We could implement a seperate table for this, in late dev this will be neccessary as someone could spam like on thier own post */
+    dataLink VARCHAR(50), /* Assuming that we will be hosting PDF's online using a API and a link to said data */
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+
+CREATE TABLE Reviews(
+    Review_ID INT AUTO_INCREMENT PRIMARY KEY,
+    SG_ID INT,
+    user_ID INT,
+    Review VARCHAR(250)
+    FOREIGN KEY (user_id) REFERENCES Users(id),
+    FOREIGN KEY (SG_ID) REFERENCES StudyGuides(SG_id)
+);
+
+
+INSERT INTO Users (username, password) VALUES ('admin', 'password123');
+
