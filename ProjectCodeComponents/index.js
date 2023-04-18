38fdd0f813e53f181c83f2b95c08c0e534a914ee
Alex <95457920+alba4576@users.noreply.github.com>
2023-04-18 16:53:43 -0600

    test2

--- ProjectCodeComponents/index.js
+++ ProjectCodeComponents/index.js
@@ -71,6 +71,13 @@ console.log(url);
 // https://res.cloudinary.com/<cloud_name>/image/upload/h_150,w_100/olympic_flag
 
 
+
+//Uploadcare:
+
+import uploadcare from 'uploadcare-widget/uploadcare.lang.en.min.js'
+
+
+
 // *****************************************************
 // <!-- Section 3 : App Settings -->
 // *****************************************************
