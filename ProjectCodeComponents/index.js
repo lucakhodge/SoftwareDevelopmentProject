69e330a6c42bd3038f14302ff0ef7f1224c608c5
Alex <95457920+alba4576@users.noreply.github.com>
2023-04-19 14:51:20 -0600

    stash

--- ProjectCodeComponents/index.js
+++ ProjectCodeComponents/index.js
@@ -42,40 +42,9 @@ db.connect()
 
 //Cloudinary API:
 
-//configuration
-cloudinary.config({
-  cloud_name: "dfx20hhlk",
-  api_key: "676725369999912",
-  api_secret: "cISXL5XpemBW1clvfGWWUJe1mqg"
-});
-
-const res = cloudinary.uploader.upload('https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg', {public_id: "olympic_flag"})
-
-res.then((data) => {
-  console.log(data);
-  console.log(data.secure_url);
-}).catch((err) => {
-  console.log(err);
-});
-
-// Generate 
-const url = cloudinary.url("olympic_flag", {
-  width: 100,
-  height: 150,
-  Crop: 'fill'
-});
-
-
-// The output url
-console.log(url);
-// https://res.cloudinary.com/<cloud_name>/image/upload/h_150,w_100/olympic_flag
-
 
 
 //Uploadcare:
-
-import uploadcare from 'uploadcare-widget/uploadcare.lang.en.min.js'
-
 
 
 // *****************************************************
